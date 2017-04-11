694c4bb036827a6004ac794ec4dc767463e89222
Rupesh Chinta <rupesh.ch97@gmail.com>
2017-04-11 00:06:04 -0400

    more tests

--- Cryptography_Python/pqBits.py
+++ Cryptography_Python/pqBits.py
@@ -2,12 +2,18 @@ from getnums import genRSAKey
 from math import log
 
 fail = 0
+otherFail = 0
+dFail = 0
 for i in range(100):
 	p, q, d, e, n = genRSAKey()
-	print(p.bit_length(), q.bit_length())
-	print(int(log(p, 2)))
-	print(int(log(q, 2)))
 	if (p.bit_length() == q.bit_length()):
 		fail+=1
+	# other standard
+	if abs(p-q) <= 2**(n.bit_length()/2-100):
+		otherFail+=1
+	if d < n/4: dFail++
+
 
 print(fail)
+print(otherFail)
+print(dFail)
