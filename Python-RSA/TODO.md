2b062e656f5f4f7f3e3e1fbbb95d4fb53de45a76
Vatsal09 <vpan17@gmail.com>
2017-04-12 04:43:45 -0400

    Code added to check the uniqueness of the mod and verify that the e and d exponenets are large

--- Python-RSA/TODO.md
+++ Python-RSA/TODO.md
@@ -7,6 +7,12 @@ Minimum deliverables:
 * varying modulus?
 * Prime Checking? 
 	
-	
+
+Key size = 2, 4, 8 too small
+Key sizes = 16+, all Public key exponents are 65537. This is because we are using the default function. 
+
+Key size 16= we do get sometimes that the private key exponent is less than 1000
+
+For larger test cases, 128 and above, we see that the n and d are large and they are non-repeating.
 
 
--- Python-RSA/keys.py
+++ Python-RSA/keys.py
@@ -1,14 +1,14 @@
 import rsa
 import math
 import time
-
+import numpy as np
 if __name__ == '__main__':
     
     
     # change h for how many p's and q's you want to generate
 
     w = 2 # 2 columns, col 1 = p and col 2= q
-    h = 1000 #sets the number of keys too be generated
+    h = 100 #sets the number of keys too be generated
     Matrix = [[0 for x in range(w)] for y in range(h)] #define matrix size to hold p's and q's
     total_bit_diff = 0
     key_size = 128 #128 bits by default
@@ -29,9 +29,20 @@ if __name__ == '__main__':
         q_bit_length = Matrix[x][1].bit_length()
         bit_diff = abs(p_bit_length - q_bit_length)
         print bit_diff
+    
 
-    # for x in xrange(h):
+    
+    # Each row of array : [public key encryption exp., public key mod., private key decryption exp., private key mod.]
+    num_col = 4 # 4 columns defined above
+    arr = [[0 for x in range(num_col)] for y in range(h)]
+    for x in xrange(h):
+        (pub_key, priv_key) = rsa.key.newkeys(key_size)
+        arr[x][0] = pub_key.e
+        arr[x][1] = pub_key.n
+        arr[x][2] = priv_key.d
+        arr[x][3] = priv_key.n
+        print("Public Key Exp. = " + str(pub_key.e) + " Public Key Mod. = " + str(pub_key.n) + '\n')
+        print("Private Key Exp. = " + str(priv_key.d) + " Private Key Mod. = " + str(priv_key.n) + '\n')
+        print("---------------------------------------------------------------------------------------")
+           
         
-    #     print(Matrix[x][0]) 
-    #     print(Matrix[x][1])
-    # 
