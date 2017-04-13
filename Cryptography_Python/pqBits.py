150a397cf206441bc4b945449dd399cdca7220b0
Rupesh Chinta <rupesh.ch97@gmail.com>
2017-04-12 23:23:07 -0400

    added repeat checks and timing for key generation

--- Cryptography_Python/pqBits.py
+++ Cryptography_Python/pqBits.py
@@ -1,5 +1,6 @@
 from getnums import genRSAKey
 from math import log
+import time
 
 # def isqrt(n):  
 #     'isqrt(n)\n\nReturn floor(sqrt(n)).'  
@@ -19,18 +20,35 @@ from math import log
 #     return result  
 
 fail = 0
+pqdup = 0
+ndup = 0
 # otherFail = 0
 # dFail = 0
-for i in range(100):
+pqset = set()
+nset = set()
+tim = 0
+for i in range(10000):
+	print(i)
+	t = time.time()
 	p, q, d, e, n = genRSAKey()
+	tim += time.time()-t
 	if (p.bit_length() == q.bit_length()):
 		fail+=1
+	if p in pqset: pqdup+=1
+	if q in pqset: pqdup+=1
+	if n in nset: ndup+=1
+	pqset.add(p)
+	pqset.add(q)
+	nset.add(n)
 	# other standard
 	# if abs(p-q) <= 2**(n.bit_length()/2-100):
 	# 	otherFail+=1
 	# if d < isqrt(isqrt(n))//3: dFail+=1
 
 
-print(fail)
+print("same bit length", fail)
+print("pqrepeats", pqdup)
+print("nrepeats", ndup)
+print("keygentime", tim)
 # print(otherFail)
 # print(dFail)
